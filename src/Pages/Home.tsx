06c058c5ee1384b67fff53e7bb0540cf4bc36332
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-06 22:25:36 +0100

    working on the search functionality

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import UserResults from '../components/users/UserResults'
+import React from "react";
+import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
 
-const Home:React.FC = () => {
+const Home: React.FC = () => {
   return (
     <>
       <UserResults />
+      <UserSearch />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
