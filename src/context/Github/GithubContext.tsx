9dec06a299f2079bc4c86d96be95d20aff6786e6
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-07 20:46:22 +0100

    worked ont he user layout

--- src/context/Github/GithubContext.tsx
+++ src/context/Github/GithubContext.tsx
@@ -10,8 +10,24 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider: React.FC<ReactNode> = ({ children }) => {
   const [isLoading, setisLoading] = useState<Boolean>(false);
   const [users, setUsers] = useState<userTypes[]>([]);
-  const [user, setUser] = useState<any>();
-
+  const [user, setUser] = useState<userTypes>({
+    login: "Godsheritage",
+    id: 56279264,
+    avatar_url: "https://avatars.githubusercontent.com/u/56279264?v=4",
+    html_url: "https://github.com/Godsheritage",
+  
+    type: "User",
+    name: "Godsheritage D Adeoye",
+    blog: "https://react-portfolio-ruddy.vercel.app/",
+    location: "Nigeria",
+    hireable: null,
+    bio: "Front End Developer || React Developer|| UI/UX Designer ||Tech Enthusiast\r\n",
+    twitter_username: "kyng_Herit",
+    public_repos: 26,
+    public_gists: 1,
+    followers: 3,
+    following: 5,
+  });
 
   // to search for users
   const searchUsers = async (text: any) => {
@@ -29,9 +45,8 @@ export const GithubProvider: React.FC<ReactNode> = ({ children }) => {
     setisLoading(false);
   };
 
-
   // to get a single user
-  const getUser = async (login: any) => {
+  const getUser = async (login: String) => {
     setisLoading(true);
 
     const response: any = await axios.get(
@@ -42,13 +57,13 @@ export const GithubProvider: React.FC<ReactNode> = ({ children }) => {
     );
 
     if (response.status === 404) {
-      window.location.href = ('/notfound');
+      window.location.href = "/notfound";
     } else {
       setUser(response.data);
+      // console.log(response.data)
       setisLoading(false);
     }
   };
-
 
   // to clear users from state
   const clearUsers = () => {
--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,29 @@
 export interface userTypes {
+  name: String;
   id: Number;
+  type: String;
+  avatar_url: string;
+  location: String;
+  bio: String;
+  blog: String;
+  twitter_username: String;
   login: String;
-  avatar_url: String;
+  html_url: string;
+  followers: Number;
+  following: Number;
+  public_repos: Number;
+  public_gists: Number;
+  hireable: null;
 }
 
 export interface contextTypes {
   isLoading: Boolean;
-  users: any;
-  user: any;
+  users: userTypes[];
+  user: userTypes;
   searchUsers: (text: any) => Promise<void>;
   clearUsers: () => void;
-  getUser : (login : any) => void
+  getUser: (login: any) => void;
 }
-
 
 export interface AlertContextTypes {
   alert: null;
